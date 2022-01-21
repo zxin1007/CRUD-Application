9ef8b5c24342473c6167f31f7be1e01e41151bcb
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 15:01:45 -0500

    Render components at corresponding URLs

--- college-management/src/index.js
+++ college-management/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import App from './components/App';
 import './index.css';
+import Campuses from "./components/Campuses";
+import Students from "./components/Students";
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="home" element={<App/>} />
+            <Route path="campuses" element={<Campuses />} />
+            <Route path="students" element={<Students />} />
+        </Routes>
     </BrowserRouter>,
     document.getElementById('root')
 );
