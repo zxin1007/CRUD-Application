18e96ade2e5f8437200b3aa988d330607883b058
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 15:57:00 -0500

    Remove hard-coded navbar

--- college-management/src/components/Campuses.js
+++ college-management/src/components/Campuses.js
@@ -4,14 +4,6 @@ import {Link} from "react-router-dom";
 function Campuses({props}) {
     return (
         <>
-            <h1>Campus Listing</h1>
-
-            <nav>
-                <Link to="/home">Home</Link> |{" "}
-                <Link to="/campuses">Campuses</Link> |{" "}
-                <Link to="/students">Students</Link>
-            </nav>
-
             <h1>All Campuses</h1>
             {/*<ul>*/}
             {/*    {props.map(campus =>*/}
--- college-management/src/components/Students.js
+++ college-management/src/components/Students.js
@@ -4,14 +4,6 @@ import {Link} from "react-router-dom";
 function Students({props}) {
     return (
         <>
-            <h1>Campus Listing</h1>
-
-            <nav>
-                <Link to="/home">Home</Link> |{" "}
-                <Link to="/campuses">Campuses</Link> |{" "}
-                <Link to="/students">Students</Link>
-            </nav>
-
             <h1>All Students</h1>
             {/*<ul>*/}
             {/*    {props.map(student =>*/}
