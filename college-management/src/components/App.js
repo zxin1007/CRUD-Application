8a61f7eb74bf2780cffe6df29e620954c875d8c2
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 02:53:55 -0500

    Move App.js to components folder

---
+++ college-management/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <>
+            </>
+        </Router>
+    );
+}
+
+export default App;
