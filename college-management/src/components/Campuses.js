e55551e097024ccb49d3992a0f06c5c150d41390
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 14:46:33 -0500

    Map through campuses and display name and image #3

--- college-management/src/components/Campuses.js
+++ college-management/src/components/Campuses.js
@@ -1,7 +1,18 @@
 import React from'react';
 
 function Campuses({props}) {
-
+    return (
+        <>
+            <h1>All Campuses</h1>
+            <ul>
+                {props.map(campus =>
+                <li>
+                    Name: {campus.name}
+                    <img src={campus.image} alt={campus.id}/>
+                </li>)}
+            </ul>
+        </>
+    );
 }
 
 export default Campuses;
