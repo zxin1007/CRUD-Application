77b4fde028569f5df6a847f2ba57d7e4ee0b7c58
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 15:04:59 -0500

    Add in navigation bar
    Commented out map function for the timebeing until I can properly pass
    props to the child component.

--- college-management/src/components/Students.js
+++ college-management/src/components/Students.js
@@ -1,15 +1,24 @@
 import React from'react';
+import {Link} from "react-router-dom";
 
 function Students({props}) {
     return (
         <>
+            <h1>Campus Listing</h1>
+
+            <nav>
+                <Link to="/home">Home</Link> |{" "}
+                <Link to="/campuses">Campuses</Link> |{" "}
+                <Link to="/students">Students</Link>
+            </nav>
+
             <h1>All Students</h1>
-            <ul>
-                {props.map(student =>
-                    <li>
-                        Name: {student.name}
-                    </li>)}
-            </ul>
+            {/*<ul>*/}
+            {/*    {props.map(student =>*/}
+            {/*        <li>*/}
+            {/*            Name: {student.name}*/}
+            {/*        </li>)}*/}
+            {/*</ul>*/}
         </>
     );
 }
