ce24cf7e22a2d0a9c72bb4538ad2bee3c865fca8
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 13:18:30 -0500

    Set component to accept props

--- college-management/src/components/Campuses.js
+++ college-management/src/components/Campuses.js
@@ -1,6 +1,6 @@
 import React from'react';
 
-function Campuses() {
+function Campuses({props}) {
 
 }
 
---
+++ college-management/src/components/Students.js
@@ -0,0 +1,8 @@
+import React from'react';
+
+function Students({props}) {
+    // return (
+    // );
+}
+
+export default Students
