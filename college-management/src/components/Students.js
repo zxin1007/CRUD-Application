2dde81d711cd8dfbd53726a4f953565e0b47eabb
Lucy Lee <gamesandhannah@gmail.com>
2022-01-22 02:39:16 -0500

    List all student names from dummy data. #4

--- college-management/src/components/Students.js
+++ college-management/src/components/Students.js
@@ -1,7 +1,13 @@
 import React from'react';
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import {getStudents} from "../data/students_data";
 
-function Students({props}) {
+function Students() {
+    let students = getStudents();
+    students.sort((a, b) =>
+        a.lastName.localeCompare(b.lastName)  // Sort alphabetically
+    );
+
     return (
         <>
             <h1>All Students</h1>
@@ -11,6 +17,20 @@ function Students({props}) {
             {/*            Name: {student.name}*/}
             {/*        </li>)}*/}
             {/*</ul>*/}
+
+            {students.map(student => (
+                <ul style={{ display: "block", margin: "1rem 0"}}>
+                    <Link
+                        style={{ display: "inline-block", margin: "1rem 0"}}
+                        to={`/students/${student.lastName}%20${student.firstName}`}
+                        key={student.name}
+                    >
+                        <li>{student.lastName}, {student.firstName}</li>
+                    </Link>
+                </ul>
+            ))}
+
+            <Outlet />
         </>
     );
 }
