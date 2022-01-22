d90d31781f53236598ccb01a69f6e29fc227b117
Lucy Lee <gamesandhannah@gmail.com>
2022-01-22 02:40:20 -0500

    Nest single-student view in all-student view route

--- college-management/src/index.js
+++ college-management/src/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import App from './components/App';
-import './index.css';
 import Campuses from "./components/Campuses";
 import Students from "./components/Students";
 import Campus from "./components/Campus";
+import Student from "./components/Student"
+import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path="home" element={<App />} />
-            <Route path="/" element={<App />}>
-                <Route path="campuses" element={<Campuses />}>
+            <Route path="/" element={<App />} >
+                <Route path="campuses" element={<Campuses />} >
                     <Route path="campusName" element={<Campus />} />
                 </Route>
-                <Route path="students" element={<Students />} />
+
+                <Route path="students" element={<Students />} >
+                    <Route path="studentName" element={<Student />} />
+                </Route>
             </Route>
         </Routes>
     </BrowserRouter>,
