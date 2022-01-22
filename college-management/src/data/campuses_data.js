5a956f871dfc8ed517622df17a8e0d086a229696
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 23:03:42 -0500

    Edit names of dummy campuses

--- college-management/src/data/campuses_data.js
+++ college-management/src/data/campuses_data.js
@@ -1,34 +1,34 @@
 let campuses = [
     {
-        name: "A",
+        name: "Campus A",
         image: "No image available",
         address: "2900 Bedford Ave, Brooklyn, NY 11210",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         students: []
     },
     {
-        name: "B",
+        name: "Campus B",
         image: "No image available",
         address: "2900 Bedford Ave, Brooklyn, NY 11210",
         description: "Malesuada fames ac turpis egestas integer eget.",
         students: []
     },
     {
-        name: "C",
+        name: "Campus C",
         image: "No image available",
         address: "2900 Bedford Ave, Brooklyn, NY 11210",
         description: "Tellus elementum sagittis vitae et leo duis ut. In dictum non consectetur a erat nam at. Id eu nisl nunc mi ipsum.",
         students: []
     },
     {
-        name: "D",
+        name: "Campus D",
         image: "No image available",
         address: "2900 Bedford Ave, Brooklyn, NY 11210",
         description: "Est ultricies integer quis auctor elit. Vitae justo eget magna fermentum iaculis eu non. Eu nisl nunc mi ipsum faucibus.",
         students: []
     },
     {
-        name: "E",
+        name: "Campus E",
         image: "No image available",
         address: "2900 Bedford Ave, Brooklyn, NY 11210",
         description: "Tempus iaculis urna id volutpat lacus laoreet.",
@@ -36,6 +36,6 @@ let campuses = [
     }
 ];
 
-export function getCampuses () {
+export function getCampuses() {
     return campuses;
 }
