661b028df534cd93c101dbd1a758ef5127b0f2ed
Lucy Lee <gamesandhannah@gmail.com>
2022-01-21 23:04:33 -0500

    List all names and images of campuses from dummy data #3

--- college-management/src/components/Campuses.js
+++ college-management/src/components/Campuses.js
@@ -1,10 +1,13 @@
 import React from'react';
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import {getCampuses} from "../data/campuses_data";
 
-function Campuses({props}) {
+function Campuses() {
+    let campuses = getCampuses();
+
     return (
         <>
-            <h1>All Campuses</h1>
+            <h2>All Campuses</h2>
             {/*<ul>*/}
             {/*    {props.map(campus =>*/}
             {/*    <li>*/}
@@ -12,6 +15,24 @@ function Campuses({props}) {
             {/*        <img src={campus.image} alt={campus.id}/>*/}
             {/*    </li>)}*/}
             {/*</ul>*/}
+
+            {campuses.map(campus => (
+                <span style={{ display: "block", margin: "1rem 0"}}>
+                    <Link
+                        style={{ display: "inline-block", margin: "1rem 0"}}
+                        to={`/campuses/${campus.name}`}
+                        key={campus.name}
+                    >
+                        {campus.name}
+                    </Link>
+
+                    <span style={{ display: "block", margin: "1rem 0"}}>
+                        {campus.image}
+                    </span>
+                </span>
+            ))}
+
+            <Outlet />
         </>
     );
 }
