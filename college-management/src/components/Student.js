2884afb9a546c3428109bfce0493c9ebffa72420
Lucy Lee <gamesandhannah@gmail.com>
2022-01-24 04:29:38 -0500

    Render properties in single-student view, closes #7

--- college-management/src/components/Student.js
+++ college-management/src/components/Student.js
@@ -1,27 +1,77 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {deleteStudent, getStudent} from "../data/students_data";
+import {useParams, useNavigate} from "react-router-dom";
 import React from "react";
+import axios from "axios";
 
 export default function Student() {
-    let navigate = useNavigate();
     let params = useParams();
-    let student = getStudent(parseInt(params.studentId), 10)
+    let navigate = useNavigate();
 
+    let studentId = parseInt(params.studentId);
+    console.log("Student ID:" + studentId)
+
+    // It's now 4:16 AM, and I'm trying to apply the Campus() logic
+    // to Student(). Pleasedon'trunintoanymorebugs.
+    const [id, setId] = React.useState("");
+    const [fname, setFname] = React.useState("");
+    const [lname, setLname] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [gpa, setGPA] = React.useState("");
+    const [campus, setCampus] = React.useState("");
+    const [img, setImg] = React.useState("");
+
+    // Load campus API from local host
+    React.useEffect(async () => {
+        try {
+            await axios.get(`http://localhost:3001/api/student/${studentId}`)
+                .then((response) => {
+                    console.log(response)
+                    setId(response.data.student.id);
+                    setFname(response.data.student.firstName)
+                    setLname(response.data.student.lastName)
+                    setEmail(response.data.student.email)
+                    setGPA(response.data.student.gpa)
+                    setCampus(response.data.student.campus)
+                    setImg(response.data.student.img)
+                });
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    const deleteStudent = (studentId) => {
+        axios.delete(`http://localhost:3001/api/campus/${studentId}`,
+            {params: {id: studentId}})
+            .then(response => {
+                console.log(response);
+            })
+    };
+
+    const noCampus = () => {
+        if (campus !== undefined) {
+            return <p>{campus}</p>
+        } else {
+            return <p>This student doesn't belong to a campus.</p>
+        }
+    }
     return (
         <>
-            <h2>{student.firstName} {student.lastName}</h2>
-            <p>{student.image}</p>
-            <h3>Email address:</h3>
-            <p>{student.email}</p>
-            <h3>GPA:</h3>
-            <p>{student.gpa}</p>
-            <h3>Campus:</h3>
-            <p>{student.campus}</p>
+            <img src={img} />
+            <h2>{fname} {lname}</h2>
+            <p>{img}</p>
+            <h3>Email</h3>
+            <p>{email}</p>
+            <h3>GPA</h3>
+            <p>{gpa}</p>
+            <h3>Campus</h3>
+            {noCampus()}
 
+
+            <div>
             <button onClick={() => {
-                    deleteStudent(student.id);
-                    navigate("/students")
-                }}>X</button> Remove Student
+                deleteStudent(id);
+                navigate("/campuses")
+            }}>X</button> Remove Campus
+            </div>
         </>
     )
 }
