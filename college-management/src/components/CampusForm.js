7a32d1f0fa2cc28e214b4687eb50d261088e5cbb
Lucy Lee <gamesandhannah@gmail.com>
2022-01-23 23:24:15 -0500

    Add form inputs that validate post request. fixes #10

--- college-management/src/components/CampusForm.js
+++ college-management/src/components/CampusForm.js
@@ -1,26 +1,92 @@
 import React from 'react'
+import axios from 'axios';
 
 export default function CampusForm() {
     const [campus, setCampus] = React.useState({
         name: "",
-        address: ""
+        address: "",
+        city: "",
+        state: "",
+        zip: ""
     });
+
+    const handleChange = (event) => {
+        setCampus(prevState => {
+            return {...campus, [event.target.name]: event.target.value}
+        })
+    }
+
+    const addCampus = (event) => {
+        event.preventDefault();
+        console.log(campus);
+
+        // let formData = new FormData();
+        //
+        // formData.append("name", campus.name);
+        // formData.append("address", campus.address);
+        //
+        // axios({
+        //     method: "POST",
+        //     url: "http://localhost:3001/api/campus/",
+        //     data: formData,
+        //     headers: {"Content-Type": "multipart/form-data"},
+        // }).then(function(response) {
+        //     console.log(response);
+        // }).catch(function(error) {
+        //     console.log(error);
+        // })
+
+        setCampus(prevState => {
+            return {...campus, id: campus.length}
+        })
+        console.log(campus);
+
+        axios.post("http://localhost:3001/api/campus", campus)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+            console.log(error);
+        });
+    }
 
     return (
         <>
             Add a new campus:
-            <form>
+            <form onSubmit={addCampus}>
                 <label>Name:<br/>
                     <input type="text"
                            name="name"
-                           onChange={event => setCampus(event.target.value)}
+                           value={campus.name}
+                           onChange={handleChange}
                     />
                 </label><br/>
-                <label>Address:<br/>
+                <label>Street address:<br/>
                     <input type="text"
                            name="address"
-                           onChange={event =>
-                               setCampus({...campus, [event.target.name]: event.target.value})}
+                           value={campus.address}
+                           onChange={handleChange}
+                    />
+                </label><br/>
+                <label>City:<br/>
+                    <input type="text"
+                           name="city"
+                           value={campus.city}
+                           onChange={handleChange}
+                    />
+                </label><br/>
+                <label>State:<br/>
+                    <input type="text"
+                           name="state"
+                           value={campus.state}
+                           onChange={handleChange}
+                    />
+                </label><br/>
+                <label>Zipcode:<br/>
+                    <input type="number"
+                           name="zip"
+                           value={campus.zip}
+                           onChange={handleChange}
                     />
                 </label><br/>
                 <input type="submit" value="Submit"/>
